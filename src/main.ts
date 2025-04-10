abb94df85d3b9494ec83c1bc88a57d525beff3f7
Manu <manuelli.flaviano@germinare.org.br>
2025-04-10 09:27:13 -0300

    Mudando CORS

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE', 
     allowedHeaders: 'Content-Type, Authorization', 
   });
